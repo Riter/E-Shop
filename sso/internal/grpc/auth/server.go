c89874e14401a714c199e83a9a561f5c7af4eb40
GGiovanni9152 <mrplyshka9152@gmail.com>
2025-03-29 17:34:56 +0300

    in progress

--- sso/internal/grpc/auth/server.go
+++ sso/internal/grpc/auth/server.go
@@ -5,19 +5,52 @@ import (
 
 	ssov1 "github.com/GGiovanni9152/protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
 }
 
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
 func Register(gRPC *grpc.Server) {
 	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
 }
 
+const (
+	emptyValue = 0
+)
+
 func (s *serverAPI) Login(
 	ctx context.Context, req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is nessesary")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
 	return &ssov1.LoginResponse{Token: req.GetEmail()}, nil
 }
 
